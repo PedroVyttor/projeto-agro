109e80fb1c6a9f206d3008642b32ce49edb4cf22
PedroVyttor <vyttinho18@gmail.com>
2026-05-18 12:49:14 -0300

    commit

---
+++ aagro.py
@@ -0,0 +1,22 @@
+#Projeto Algoritmos
+
+while True:
+    select = int(input(
+        'Bem-vindo ao Agro(inventar um nume ai)\n1- Opção\n'
+        '2- Opção\n'
+        '3- Opção\n'
+        '4- Opção\n'
+        '0- Sair\n\n'
+        'Selecione a opção: '))
+
+    if select == 1:
+        print('UM')
+
+    elif select == 2:
+        print('DOIS')
+
+    elif select == 3:
+        print('TRES')
+
+    elif select == 4:
+        print('QUATRO')
